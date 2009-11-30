44adc54cad963a20db1df1bb164852f9af5740ac
Shailesh Patil <pshailesh1612@gmail.com>
2009-11-30 11:30:10 +0530

    moved the generators to the parent folder and added new script

---
+++ load.rb
@@ -0,0 +1,15 @@
+require 'config/environment.rb'
+require 'mysql'
+
+system('nohup ruby script/generate employee_migration &')
+system('nohup rake db:migrate &')
+
+mysql = Mysql.init()
+
+# change  the mysql connection parameters accordingly
+mysql.connect('localhost', 'root', '', 'ejosh_dev1')
+
+mysql.query("update extensions set entry_point = 'employees' where id = "+ ARVG +" ;")
+
+mysql.close()
+
