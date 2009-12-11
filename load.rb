b748be8db7aabdb3de9e04046d7c8278daee532a
Shailesh Patil <pshailesh1612@gmail.com>
2009-12-11 11:04:43 +0530

    changed the username and password

--- load.rb
+++ load.rb
@@ -7,7 +7,7 @@ system('nohup rake db:migrate &')
 mysql = Mysql.init()
 
 # change  the mysql connection parameters accordingly
-mysql.connect('localhost', 'root', '', 'ejosh_dev1')
+mysql.connect('localhost', 'ejosh', 'josh123@', 'eJosh')
 
 mysql.query("update extensions set entry_point = '/employees' where id = #{ARGV} ;")
 
